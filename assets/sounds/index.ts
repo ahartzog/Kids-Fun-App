19484fb148bd8623d306384468547fa185a49e05
Alek Hartzog <ahartzog@gmail.com>
2021-11-23 19:23:37 -0500

    Add elephant sound

--- assets/sounds/index.ts
+++ assets/sounds/index.ts
@@ -1,5 +1,5 @@
 const fart_1 = require('./fart1.mp3');
 const airline_call_beep = require('./airline_call_beep.mp3');
+const elephant_bellow = require('./elephant_bellow.mp3');
 
-
-export {fart_1, airline_call_beep}
+export {fart_1, airline_call_beep, elephant_bellow}
